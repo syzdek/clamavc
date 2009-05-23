4403d078b786692b5308b4a8c2cc934961dafbca
David M. Syzdek <david.syzdek@acsalaska.net>
2009-05-23 07:32:50 -0800

    Updating to read a file into a buffer for instream

--- src/clamavc-instream.c
+++ src/clamavc-instream.c
@@ -32,6 +32,7 @@
 ///////////////
 
 #include <clamavc.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -59,9 +60,12 @@ int main PARAMS((int argc, char * argv[]));
 /// @param[in]  argv     array of arguments passed to clamavclient
 int main (int argc, char * argv[])
 {
+   int            fd;
+   int            len;
    int32_t        err;
    CLAMAVC      * clamp;
    const char   * file;
+   char           buff[4096];
 
 #ifdef HAVE_GETTEXT
    setlocale (LC_ALL, "");
@@ -75,23 +79,56 @@ int main (int argc, char * argv[])
       return(0);
 
    if (clamavc_ping(clamp))
+   {
       perror("clamavc_ping()");
+      clamavc_close(clamp);
+      return(1);
+   };
 
-   if ((err = clamavc_instream(clamp, "/tmp/test/", 10)) == -1)
-      perror("clamavc_instream()");
-   if ((err = clamavc_instream(clamp, "/tmp/test/", 10)) == -1)
-      perror("clamavc_instream()");
-   //if ((err = clamavc_instream(clamp, "/tmp/test/", 10)) == -1)
-   //   perror("clamavc_instream()");
-   if ((err = clamavc_instream(clamp, NULL, 0)) == -1)
-      perror("clamavc_instream()");
-   //if (err)
-   //   printf("infected\n");
-   //else
-   //   printf("not infected\n");
+   if ((fd = open(file, O_RDONLY)) == -1)
+   {
+      perror("open()");
+      clamavc_close(clamp);
+      return(1);
+   };
+
+   while((len = read(fd, buff, 4096)) > 0)
+   {
+      if ((err = clamavc_instream(clamp, buff, (unsigned)len)) == -1)
+      {
+         perror("clamavc_instream()");
+         close(fd);
+         clamavc_close(clamp);
+         return(1);
+      };
+   };
+   if (len == -1)
+   {
+      perror("read()");
+      close(fd);
+      clamavc_close(clamp);
+      return(1);
+   };
+
+   switch(err = clamavc_instream(clamp, NULL, 0))
+   {
+      case -1:
+         perror("clamavc_instream()");
+         break;
+      case 0:
+         printf("not infected\n");
+         break;
+      default:
+         printf("infected\n");
+         break;
+   };
+
+   close(fd);
 
    clamavc_close(clamp);
 
+   if (err)
+      return(1);
    return(0);
 }
 
