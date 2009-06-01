51c502c795144e9bec684daaa3d8b0dbea75e28a
David M. Syzdek <david.syzdek@acsalaska.net>
2009-06-01 01:53:25 -0800

    Fixing clamavc_instream() to use IDSESSION

--- lib/libclamavc.c
+++ lib/libclamavc.c
@@ -445,7 +445,7 @@ int32_t clamavc_instream(CLAMAVC * clamp, const char * src, size_t nbyte)
    uint32_t pos;
    uint32_t size;
 
-   if (clamavc_connect(clamp, 0))
+   if (clamavc_connect(clamp, 1))
    {
       clamp->instream = 0;
       return(-1);
@@ -512,6 +512,8 @@ int32_t clamavc_instream(CLAMAVC * clamp, const char * src, size_t nbyte)
 
    // process result from clamd
    for(offset = 0; ( (buff[offset]) && (buff[offset] != ':') ); offset++);
+   if ((buff[offset]))
+      for(offset = offset+1; ( (buff[offset]) && (buff[offset] != ':') ); offset++);
    if (!(buff[offset]))
    {
       clamavc_reset(clamp);
@@ -536,9 +538,9 @@ int32_t clamavc_instream_file(CLAMAVC * clamp, const char * file)
    int            fd;
    int            len;
    int32_t        err;
-   char           buff[4096];
+   char           buff[1024];
 
-   if (clamavc_connect(clamp, 0))
+   if (clamavc_connect(clamp, 1))
    {
       clamp->instream = 0;
       return(-1);
@@ -547,7 +549,7 @@ int32_t clamavc_instream_file(CLAMAVC * clamp, const char * file)
    if ((fd = open(file, O_RDONLY)) == -1)
       return(-1);
 
-   while((len = read(fd, buff, 4096)) > 0)
+   while((len = read(fd, buff, 1024)) > 0)
    {
       if ((err = clamavc_instream(clamp, buff, (unsigned)len)) == -1)
       {
@@ -564,8 +566,6 @@ int32_t clamavc_instream_file(CLAMAVC * clamp, const char * file)
    close(fd);
 
    return(clamavc_instream(clamp, NULL, 0));
-
-   return(0);
 }
 
 
